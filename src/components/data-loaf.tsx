9b2275cb0f0687802c1e5dc3388a7b1af71b9d40
Brandon Corey <bcorey3660@gmail.com>
2024-05-01 04:52:02 -0400

    general tailwindcss updates for styling

--- src/components/data-loaf.tsx
+++ src/components/data-loaf.tsx
@@ -4,17 +4,19 @@ import infraPreview from "../../public/dataloaf-infra.png";
 import dashboardPreview from "../../public/dataloaf-dashboard.png";
 export default function DataLoafPreview(): JSX.Element {
   return (
-    <section className="w-full bg-gray-100 py-12 md:py-24 lg:py-32 dark:bg-gray-800">
+    <section
+      id="dataloaf"
+      className="w-full bg-gray-100 py-12 md:py-24 lg:py-32 dark:bg-gray-800"
+    >
       <div className="container m-auto px-4 md:px-6">
-        <div className="space-y-4 text-center">
+        <div className="flex flex-col items-center gap-5 text-center">
           <div className="space-y-2">
             <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
               DataLoaf
             </h2>
-            <p className="mx-auto max-w-[900px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+            <p className="mx-auto max-w-[900px] pb-6 pt-2 text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
               Recently, I worked on developing DataLoaf. An open source, self
-              hosted product analytics platform for developers that deploys to
-              AWS.
+              hosted product analytics tool for developers that deploys to AWS.
             </p>
           </div>
           <Image
@@ -35,9 +37,11 @@ export default function DataLoafPreview(): JSX.Element {
             src={infraPreview}
             width="1200"
           />
+
           <Link
-            className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-6 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
-            href="#"
+            className="mt-6 inline-flex h-10 w-fit items-center justify-center rounded-md bg-gray-900 px-6 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-opacity-80 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+            href="https://data-loaf.com/docs/category/case-study"
+            target="_blank"
           >
             Read the Case Study
           </Link>
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 import scrollToSection from "@/lib/smooth-scroll";
 
 export default function Hero(): JSX.Element {
@@ -7,7 +8,7 @@ export default function Hero(): JSX.Element {
       className="flex flex-1 flex-col items-center justify-center py-12 md:py-24 lg:py-32 xl:py-48"
       id="about"
     >
-      <div className="container space-y-4 px-4 text-center md:px-6">
+      <div className="container animate-fadeIn space-y-4 px-4 text-center ease-in md:px-6">
         <div className="space-y-2">
           <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl">
             Brandon Corey
@@ -16,25 +17,34 @@ export default function Hero(): JSX.Element {
             Software Engineer | Full-Stack Developer
           </p>
           <p className="mx-auto max-w-[700px] py-5 text-gray-500 md:text-xl dark:text-gray-400">
-            I am a software engineer with a background in analytics. I enjoy
-            working with large amounts of data, and I&apos;m always eager to
-            jump into new problem domains and techonologies.
+            I&apos;m a software engineer with a background in analytics, based
+            in the Boston area. I enjoy working with large amounts of data.
+            Recently, I created{" "}
+            <a
+              href="https://data-loaf.com"
+              className="font-semibold text-[#d2b573] underline"
+              target="_blank"
+            >
+              DataLoaf
+            </a>
+            , a self hosted product analytics tool.
           </p>
         </div>
         <div className="space-x-4">
           <Link
-            className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-6 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-gray-900/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
+            className="inline-flex h-10 items-center justify-center rounded-md bg-gray-900 px-6 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-opacity-80 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300"
             href="#projects"
-            onClick={(e) => scrollToSection(e, "projects")}
+            onClick={(e) => scrollToSection(e, "dataloaf")}
           >
             View Projects
           </Link>
           <Link
-            className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-white px-6 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
-            href="#contact"
-            onClick={(e) => scrollToSection(e, "contact")}
+            className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 bg-loaf px-6 text-sm font-medium shadow-sm transition-colors hover:bg-loaf hover:bg-opacity-80 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
+            href="/Brandon_Corey_Resume_2024.pdf"
+            target="_blank"
           >
-            Contact Me
+            Download Resume{" "}
+            <Image src="/noun-layers.svg" alt="" width={30} height={30} />
           </Link>
         </div>
       </div>
