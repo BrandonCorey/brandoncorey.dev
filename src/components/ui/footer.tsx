e6fa03d084d8afa54c67d1af60be83e2d455971f
Brandon Corey <bcorey3660@gmail.com>
2024-05-02 02:34:30 -0400

    give footer more top and bottom padding

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -5,13 +5,13 @@ import connectIcons from "../../../static/connect-icons";
 export default function Connect(): JSX.Element {
   return (
     <footer
-      className="flex flex-col gap-5 pb-2 pt-5 dark:bg-zinc-950"
+      className="flex flex-col gap-6 pb-2 pt-10 dark:bg-zinc-950"
       id="footer"
     >
       <h2 className="text-center text-3xl font-bold dark:text-white">
         Lets Connect!
       </h2>
-      <div className="flex justify-center gap-6">
+      <div className="flex justify-center gap-6 pb-6">
         <Link
           href="https://github.com/BrandonCorey"
           className="connect-icons"
@@ -30,7 +30,7 @@ export default function Connect(): JSX.Element {
           <connectIcons.MailIcon height={30} width={30} />
         </Link>
       </div>
-      <p className="pl-2 text-xs text-gray-500 dark:text-gray-400">
+      <p className="pl-2 text-center text-xs text-gray-500 sm:text-left dark:text-gray-400">
         © 2024 Brandon Corey. All rights reserved.
       </p>
     </footer>
