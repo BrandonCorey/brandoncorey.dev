f0227009afa78dda9cc67dbe4a39c258a7e4bc37
Brandon Corey <bcorey3660@gmail.com>
2024-06-10 03:01:46 -0400

    add dependencies needed to add blog to personal site

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextMDX from "@next/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
+import remarkGfm from "remark-gfm";
 
-export default nextConfig;
+/** @type {import('rehype-pretty-code').Options} */
+const options = {
+  theme: "catppuccin-mocha",
+};
+
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [[rehypePrettyCode, options]],
+  },
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+};
+
+export default withMDX(nextConfig);
