5388aacb79b76646e1728c345416d6da511a98e0
Brandon Corey <bcorey3660@gmail.com>
2024-06-10 03:50:51 -0400

    remove blog image preview when on small screens

--- src/components/blog-listing.tsx
+++ src/components/blog-listing.tsx
@@ -27,7 +27,7 @@ export default function BlogListing({
           alt="blog preview image"
           width={100}
           height={100}
-          className="w-auto rounded-md"
+          className="hidden w-auto rounded-md sm:inline-block"
         />
       </div>
     </>
